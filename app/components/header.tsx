3599946a62e955e92d96802a13d685ac8e570472
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-22 15:56:16 +0300

    feat: add header component

---
+++ app/components/header.tsx
@@ -0,0 +1,23 @@
+"use client";
+import styles from "@/styles/header.module.scss";
+import { Toggle } from "@/components/toggle";
+import { GitHubLogoIcon, PersonIcon } from "@radix-ui/react-icons";
+export function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.right}>
+        <a
+          href="https://github.com/ciobanunicolae/timer"
+          title="View GitHub repository"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+          aria-label="GitHub"
+        >
+          <GitHubLogoIcon />
+        </a>
+        <Toggle />
+      </div>
+    </div>
+  );
+}
